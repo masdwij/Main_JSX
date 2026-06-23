18c8383093683a493bc2abd6bfae47b69cdd0cc2
Aurelino Stanta Anka <MasdwijGibus@gmail.com>
2026-06-23 08:29:22 +0700

    react router dom

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,23 @@
 import "./App.css";
 import SignIn from "./pages/SignIn";
 import SignUpPage from "./pages/signUp";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Error from "./pages/Error";
+import { createBrowserRouter, RouterProvider, Link } from "react-router-dom";
 
 function App() {
   const myRouter = createBrowserRouter([
     {
       path: "/",
-      element: <div>Halaman Utama</div>,
+      element: 	<div className="flex justify-center items-center min-h-screen">
+          <Link to="/login" className="p-2 m-5 bg-primary text-white">
+            Login
+          </Link>
+          |
+          <Link to="/register" className="p-2 m-5 bg-primary text-white">
+            Register
+          </Link>
+        </div>,
+      errorElement: <Error />
     },
     {
       path: "/Login",
--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-//import App from './App.jsx'
+import App from './App.jsx'
 //import Exercise from "./Latihan/Exercise.jsx"
-import Form from "./Latihan/Form.jsx";
+//import Form from "./Latihan/Form.jsx";
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-   <Form />
+   <App />
   </StrictMode>,
 )
